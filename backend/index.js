1a59b14f9db62f39972a2faf776cd2e798b84c6d
unknown <dennis_otu@live.com>
2026-04-30 10:08:48 +0000

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,12 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
+
+const PORT =  5000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
@@ -11,12 +16,17 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(join(__dirname, 'public')))
 
+mongoose.connect(process.env.MONGODB_URI, { dbName: 'jist' })
+  .then(() => console.log('DB Connected'))
+  .catch(err => console.log(err)
+)
+
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-    console.log(`${socket.id} connected`);
+    console.log(`${socket.id} Connected`);
 
     socket.on('chat message', (data) => {
         console.log(`Message from ${data.name}: ${data.message}`);
@@ -25,10 +35,10 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log(`${socket.id} disconnected`);
+        console.log(`${socket.id} Disconnected`);
     });
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
