409b717d43cf4bb3997ec6b80e509a9979a5ac21
unknown <dennis_otu@live.com>
2026-05-10 03:11:40 +0000

    added username and number input fields on homepage

--- jistclient/src/app/index.tsx
+++ jistclient/src/app/index.tsx
@@ -1,16 +1,68 @@
-//import { globalStyles } from '@/styles/global';
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, TextInput, View, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
+  const [ userName, setUserName] = useState('');
+  const [ userNum, setUserNum] = useState('');
+
+  const handleSignIn = async () => {
+    console.log(`User Name: ${userName}`);
+    console.log(`User Number: ${userNum}`);
+    const formData = new FormData();
+    formData.append(userName, userNum);
+
+    try {
+      const res = await fetch('http://localhost:5000/api/v1/auth/signup', {
+        method:  'POST',
+        body: formData,
+				headers: {'Content-Type': 'multipart/form-data'},
+        credentials: 'include'
+      })
+      
+      const data = await res;
+    
+      if (data) {
+        console.log(`${data.json()}`)
+      }
+    } catch (error) {
+        console.log(error)
+    }
+    setUserName('');
+    setUserNum('');
+  }
+
   return (
-      <View style={{ flex: 1, backgroundColor: '#007bff', justifyContent: 'center' }}>
-        <Text style={{ textAlign: 'center', fontSize: 60, fontWeight: 900, color:  'white'}}>
-          JIST
-        </Text>
-        <Link href='/chatListView' style={{ textAlign: 'center', fontSize: 18, color:  'white', marginTop: 30, paddingLeft: 15 }}>
-          Chats
-        </Link>
-      </View>
+    <View style={{ flex: 1, backgroundColor: '#007bff', justifyContent: 'center' }}>
+      <Text style={{ textAlign: 'center', fontSize: 60, fontWeight: 900, color:  'white'}}>
+        JIST
+      </Text>
+      <TextInput style={styles.singInInpt} value={userName} onChangeText={(text)=>{setUserName(text)}} placeholder='Name'/>
+      <TextInput style={styles.singInInpt} value={userNum} onChangeText={(text)=>{setUserNum(text)}} placeholder='Number'/>
+      <Link href='/chatListView' style={styles.signInLink} onPress={handleSignIn}>
+        Sign In
+      </Link>
+    </View>
   );
 }
+
+const styles = StyleSheet.create ({
+  singInInpt: {
+    width: '80%',
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    marginBlock: 10,
+    backgroundColor: '#fff',
+  },
+  signInLink: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color:  'white',
+    marginTop: 20,
+  }
+});
